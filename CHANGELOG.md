8cb736243f3518955e0fbac82a91277a95aa323e
Andreas Fehn <fehnomenal@fehn.systems>
2024-04-06 00:44:49 +0200

    v1.0.0 @ 5a964ad9703a139b607846788e970ec5f1d6c6aa

--- CHANGELOG.md
+++
@@ -1,7 +0,0 @@
-# sveltekit-gen-routes
-
-## 1.0.0
-
-### Major Changes
-
-- Create initial implementation
---
+++ dist/code.d.ts
@@ -0,0 +1,4 @@
+import type { Route, RoutesConfig } from './types.js';
+export declare const helperCodeLines: string[];
+export declare const getIndexCodeLines: (routes: Route[], config: RoutesConfig, moduleName: string) => string[];
+export declare const getRouteKeyCodeLines: (routes: Route[], config: RoutesConfig, moduleName: string) => string[];
