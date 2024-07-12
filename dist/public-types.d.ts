cde6c66c8797a3e8ac98545a5085895f9b65b6db
Andreas Fehn <fehnomenal@fehn.systems>
2024-07-12 16:45:54 +0200

    v3.0.2 @ 9aac58133c6b2803d8a41cb668e1087e8f867470

---
+++ dist/public-types.d.ts
@@ -0,0 +1,6 @@
+/// <reference types="node" />
+/// <reference types="bun-types" />
+import type { base } from '$app/paths';
+export type Base = typeof base;
+export type ParamOfMatcher<T extends (...args: any) => any> = T extends (param: any) => param is infer P ? P : string;
+export type QueryParams = URLSearchParams | Record<string, string | string[] | undefined> | [string, string | string[] | undefined][];
