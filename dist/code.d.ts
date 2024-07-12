5fb8c86ac04163baa5e1f75a85e27bc2629f3a6b
Andreas Fehn <fehnomenal@fehn.systems>
2024-07-12 16:31:15 +0200

    v3.0.1 @ c309c32f23b838dafd97ca9ee1d8011686257c47

---
+++ dist/code.d.ts
@@ -0,0 +1,3 @@
+import type { Route, RoutesConfig } from './types.js';
+export declare const getIndexCodeLines: (routes: Route[], config: RoutesConfig, moduleName: string) => string[];
+export declare const getRouteKeyCodeLines: (routes: Route[], config: RoutesConfig) => string[];
